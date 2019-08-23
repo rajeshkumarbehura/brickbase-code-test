4b8c23977d49ced4ce7de8d6f340781d65321548
rajesh <rajesh_kumar_behura>
2019-08-23 17:12:28 +0800

    api renamed

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const PORT = process.env.PORT || 5000;
 
 
 app
-//.use(bodyParser.urlencoded({extended: true}),)
+    .use(bodyParser.urlencoded({extended: true}),)
     .use(bodyParser.json())
-    .use("/api", eventRoutes)
+    .use("/api/v1", eventRoutes)
     .use(function (req, res) {
         return res.status(404).send({message: 'Route' + req.url + ' Not found.'});
     })
--- routes/events.js
+++ routes/events.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const EventController = require('../controllers/EventController');
 
-router.route('/event').post(EventController.addEvent);
-router.route('/event').get(EventController.getAllEvents);
+router.route('/events').post(EventController.addEvent);
+router.route('/events').get(EventController.getAllEvents);
 router.route('/test').get(EventController.testEvent);
 
 module.exports = router;
